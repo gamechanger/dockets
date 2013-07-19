91b00538a2fe357ab77bef16460ab8a21e910ec5
Tom Leach <tom@gc.io>
2013-07-19 15:58:37 -0400

    Cut new version 0.0.35a (this thing will never come out of alpha)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setuptools.setup(
     name="Dockets",
-    version="0.0.34a",
+    version="0.0.35a",
     author="Doug Woos",
     author_email="doug@gamechanger.io",
     description="SUPER simple Redis-backed queueing in Python",
