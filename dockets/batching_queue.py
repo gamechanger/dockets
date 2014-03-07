4d3fbfcdcf2e34c12734c005994a7ff8b80607cb
thieman <travis.thieman@gmail.com>
2014-03-07 11:50:10 -0500

    Make a big function a little smaller

--- dockets/batching_queue.py
+++ dockets/batching_queue.py
@@ -20,15 +20,8 @@ def create_batching_queue(superclass):
             self._batch_size = kwargs.get('batch_size') or 10
             super(BatchingQueue, self).__init__(*args, **kwargs)
 
-        def run_once(self, worker_id):
-            """
-            Run the queue for one step.
-            """
+        def gather_envelopes(self, pipeline, worker_id):
             envelopes = []
-            worker_recorder = WorkerMetadataRecorder(self.redis, self._queue_key(),
-                                                     worker_id)
-            # The Big Pipeline
-            pipeline = self.redis.pipeline()
             while len(envelopes) < self._batch_size:
                 envelope = self.pop(worker_id, pipeline=pipeline)
                 if not envelope:
@@ -40,12 +33,9 @@ def create_batching_queue(superclass):
                                              response_time=response_time,
                                              pipeline=pipeline)
                 envelopes.append(envelope)
+            return envelopes
 
-            if not envelopes:
-                pipeline.execute()
-                return None
-
-            # clear expired envelopes
+        def clear_expired_envelopes(self, worker_recorder, pipeline, envelopes):
             envelopes_to_process = list(envelopes)
             for envelope in envelopes:
                 if envelope['ttl'] and (envelope['first_ts'] + envelope['ttl'] < time.time()):
@@ -55,6 +45,20 @@ def create_batching_queue(superclass):
                                                     pipeline=pipeline,
                                                     pretty_printed_item=self.pretty_printer(envelope['item']))
                     worker_recorder.record_expire(pipeline=pipeline)
+            return envelopes_to_process
+
+        def run_once(self, worker_id):
+            """
+            Run the queue for one step.
+            """
+            worker_recorder = WorkerMetadataRecorder(self.redis, self._queue_key(),
+                                                     worker_id)
+            pipeline = self.redis.pipeline()
+            envelopes = self.gather_envelopes(pipeline, worker_id)
+            if not envelopes:
+                pipeline.execute()
+                return None
+            envelopes = self.clear_expired_envelopes(worker_recorder, pipeline, envelopes)
 
             def handle_error(envelope):
                 self._event_registrar.on_error(item=envelope['item'],
@@ -64,7 +68,7 @@ def create_batching_queue(superclass):
                 worker_recorder.record_error(pipeline=pipeline)
                 self.error_queue.queue_error(envelope)
 
-            for envelope in envelopes_to_process:
+            for envelope in envelopes:
                 item_error_classes = self.error_classes_for_envelope(envelope)
                 try:
                     self.process_item(envelope['item'])
