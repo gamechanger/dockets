c78aeb7045db8487c5f360dc673d0e979fc1dbd2
Tom Leach <tom@gc.io>
2014-08-19 09:41:10 -0400

    0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setuptools.setup(
     name="Dockets",
-    version="0.0.45a",
+    version="0.1.0",
     author="Doug Woos",
     author_email="doug@gamechanger.io",
     description="SUPER simple Redis-backed queueing in Python",
